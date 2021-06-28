f292e0bf81d5f122b0c129f05f417a1ba09a744d
Hafiz Suleman <suleman@3-tree.com>
2021-06-28 17:44:01 +0500

    modal css issue resolve

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Modal.css';
-
+import { FaTimes } from 'react-icons/fa';
 import { Modal } from 'react-bootstrap';
 
 const CustomModal = (props) => {
@@ -12,10 +12,14 @@ const CustomModal = (props) => {
     return(
 
         <Modal show={show} onHide={handleClose} centered 
-        backdrop="static">
+        backdrop="static" focus={false}>
             
-            <Modal.Body className="px-0 py-4 bg-secondary rounded" style={{ marginLeft: '1px' }}>
-                
+            <div className="px-0 py-4 bg-secondary rounded position-relative"
+            style={{ margin: '2px' }}>
+    
+                <FaTimes className="position-absolute text-white cursor-pointer-sort" 
+                    style={{ right: '10px', top: '10px' }} onClick={handleClose} />
+
                 <div className="app-flex-column w-100 py-3 bg-secondary rounded m-0 px-0">
 
                     <h4 className="text-center text-white">Total: 100 ETH</h4>
@@ -30,7 +34,7 @@ const CustomModal = (props) => {
                     </button>
                 </div>
 
-            </Modal.Body>
+            </div>
 
         </Modal>
 
