11165d983fcf4858e5144a9cbbbd64b1ac124493
Hafiz Suleman <suleman@3-tree.com>
2021-06-28 14:52:44 +0500

    dashboard design

--- src/screens/Dashboard/Dashboard.js
+++ src/screens/Dashboard/Dashboard.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './Dashboard.css';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
+import { Row, Col } from 'react-bootstrap';
 
 const Dashboard = () => {
+
+    const data = [
+        { heading: 'ICO Start Date', date: 'Jun 2, 2021' },
+        { heading: 'ICO End Date', date: 'Oct 4, 2021' },
+        { heading: 'Total Tokens ETH', date: '3.0611162 DASH' },
+        { heading: 'ICO Start Date', date: '0xB3775fB83F7D1' },
+        { heading: 'Convert Price', date: '$800.056' },
+        { heading: 'Marget Value', date: '$780.00' },
+        { heading: 'Remaining Token', date: '25 ETH' }
+    ];
 
     return(
 
@@ -11,9 +22,25 @@ const Dashboard = () => {
             
             <Sidebar />
 
-            <div className="w-100 h-100 bg-primary" style={{ padding: '30px 50px' }}>
+            <div className="w-100 h-100 bg-primary app-flex-column" style={{ padding: '50px 50px 10px 50px' }}>
 
-                <div></div>
+                <span className="dashboard-heading-large">Dashboard</span>
+                <span className="dashboard-heading-small">Select below assets to check token details.</span>
+
+                <div className="mx-0 mt-5 w-100 p-0 text-white" style={{ height: '80%', overflowY: 'auto' }}>
+
+                    <div className="app-flex-row w-100 flex-wrap align-items-center justify-content-between">
+                    {
+                        data?.map((coins, index) => (
+                            <div className="dashboard-conis mb-4" key={index}>
+                                <span>{coins.heading}</span>
+                                <span>{coins.date}</span>
+                            </div>
+                        ))
+                    }
+                    </div>
+
+                </div>
 
             </div>
 
