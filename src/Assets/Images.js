757e5b27ce9e1ae0f5e17098509219cb90a2502e
Hafiz Suleman <suleman@3-tree.com>
2021-06-28 20:06:35 +0500

    features icons added

--- src/Assets/Images.js
+++ src/Assets/Images.js
@@ -3,5 +3,13 @@ export const Images = {
     avatar: require('./images/avatar.png').default,
     bgShape: require('./images/bg-shape.png').default,
     shapeLogo: require('./images/shape-logo.png').default,
-    bgHeader: require('./images/bg-header.png').default
+    bgHeader: require('./images/bg-header.png').default,
+
+    // features icons
+    bulb: require('./images/yellow-bulb.png').default,
+    db: require('./images/yellow-db.png').default,
+    jig: require('./images/yellow-jigsaw.png').default,
+    line: require('./images/yellow-line.png').default,
+    mind: require('./images/yellow-mind.png').default,
+    profit: require('./images/yellow-profit.png').default
 }
