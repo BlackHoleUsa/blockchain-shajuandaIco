0cf5e26f0f07a142bd2190b500665664658c028e
Hafiz Suleman <suleman@3-tree.com>
2021-08-07 16:50:28 +0500

    homepage comment out

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './global.css';
 import Shajuanda from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
@@ -37,4 +37,8 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+//  "homepage": "http://blackhole@sardartufani.com/projects/kyle-ico",
+//   "homepage": "http://sarashaj@sa-ra.digital/saratoken",
+
 reportWebVitals();
